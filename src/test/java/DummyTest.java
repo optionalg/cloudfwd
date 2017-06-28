21afc4f427fc0b6cdd8eea42cd192c1a3482f21e
Geoffrey Hendrey <ghendrey@splunk.com>
2017-06-28 16:48:29 -0700

    works with the automatic polling stuff

--- src/test/java/DummyTest.java
+++ src/test/java/DummyTest.java
@@ -16,31 +16,37 @@ import org.junit.Test;
  */
 public class DummyTest {
 
-	private static final Logger LOG = LogManager.getLogger("splunk.log4j");
+  private static final Logger LOG = LogManager.getLogger("splunk.log4j");
 
-	@Test
-	public void testHec() throws InterruptedException {
-		HttpEventCollectorErrorHandler.onError(new HttpEventCollectorErrorHandler.ErrorCallback() {
-			public void error(final List<HttpEventCollectorEventInfo> data, final Exception ex) {
-				System.out.println(ex);
-			}
-		});		
-		System.out.println("bork");
-		SplunkCimLogEvent e = new SplunkCimLogEvent("Event name", "event-id") {
-			{
-				// You can add an arbitrary key=value pair with addField.
-				addField("name", "value");
+  @Test
+  public void testHec() throws InterruptedException {
+    HttpEventCollectorErrorHandler.onError(
+            new HttpEventCollectorErrorHandler.ErrorCallback() {
+      public void error(final List<HttpEventCollectorEventInfo> data,
+              final Exception ex) {
+        System.out.println(ex);
+      }
+    });
 
+    System.out.println("bork");
+    SplunkCimLogEvent e = new SplunkCimLogEvent("Event name", "event-id") {
+      {
+        // You can add an arbitrary key=value pair with addField.
+        addField("name", "value");
 
-				// SplunkCimLogEvent provides lots of convenience methods for
-				// fields defined by Splunk's Common Information Model. See
-				// the SplunkCimLogEvent JavaDoc for a complete list.
-				setAuthAction("deny");
-			}
-		};
-		LOG.info(e);
-		LOG.error("FPOOOOOO");
-		Thread.sleep(10000);
-		
-	}
+        // SplunkCimLogEvent provides lots of convenience methods for
+        // fields defined by Splunk's Common Information Model. See
+        // the SplunkCimLogEvent JavaDoc for a complete list.
+        setAuthAction("deny");
+      }
+    };
+    LOG.info(e);
+    LOG.error("FPOOOOOO");
+    LOG.error("bar");
+    LOG.error("baz");
+    LOG.error("zap");
+
+    Thread.sleep(10000);
+
+  }
 }
