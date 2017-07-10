579138a8ef9be88c6ab4887c72b20f75d087b822
Geoffrey Hendrey <ghendrey@splunk.com>
2017-07-10 06:28:50 -0700

    UNTESTED INCOMPLETE but basic structure is there for load balancing across channels. Each LoadBalancer.send invocation will cause ChannelMetrics to update. The LoadBalancer observs all ChannelMetrics and tracks the bestChannel. LoadBalancer.send blocks if the bestChannel is unavailable, and unblocks when it becomes available.

--- src/test/java/AckTest.java
+++ src/test/java/AckTest.java
@@ -1,33 +1,31 @@
 /*
  * Proprietary and confidential. Copyright Splunk 2015
  */
+import com.splunk.logging.EventBatch;
 import com.splunk.logging.HttpEventCollectorErrorHandler;
-import com.splunk.logging.HttpEventCollectorEventInfo;
 import com.splunk.logging.SplunkCimLogEvent;
-import java.util.List;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-import org.junit.Assert;
 import org.junit.Test;
 
 /**
- *
+ * FIXME TODO make this an actual test with assertions
  * @author ghendrey
  */
-public class DummyTest {
-
+public class AckTest {
+  
   private static final Logger LOG = LogManager.getLogger("splunk.log4j");
-
+  
   @Test
   public void testHec() throws InterruptedException {
     HttpEventCollectorErrorHandler.onError(
             new HttpEventCollectorErrorHandler.ErrorCallback() {
-      public void error(final List<HttpEventCollectorEventInfo> data,
+      public void error(final EventBatch data,
               final Exception ex) {
         System.out.println(ex);
       }
     });
-
+    
     System.out.println("bork");
     SplunkCimLogEvent e = new SplunkCimLogEvent("Event name", "event-id") {
       {
@@ -40,13 +38,16 @@ public class DummyTest {
         setAuthAction("deny");
       }
     };
-    LOG.info(e);
-    LOG.error("FPOOOOOO");
-    LOG.error("bar");
-    LOG.error("baz");
-    LOG.error("zap");
-
+    for (int i = 0; i < 5; i++) {
+      LOG.info(e);
+      LOG.error("FPOOOOOO");
+      LOG.error("bar");
+      LOG.error("baz");
+      LOG.error("zap");
+      Thread.sleep(1000);
+    }
+    
     Thread.sleep(10000);
-
+    
   }
 }
