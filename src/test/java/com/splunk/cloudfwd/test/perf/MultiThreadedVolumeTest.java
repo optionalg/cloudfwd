ad3d88f54c42e47f5cf09be5861a144d6aef9718
Eric Prokop <ecprokop@gmail.com>
2017-12-14 09:41:06 -0800

    use 1KB events in multi-threaded-volume-test

--- src/test/java/com/splunk/cloudfwd/test/perf/MultiThreadedVolumeTest.java
+++ src/test/java/com/splunk/cloudfwd/test/perf/MultiThreadedVolumeTest.java
@@ -48,7 +48,7 @@ public class MultiThreadedVolumeTest extends AbstractPerformanceTest {
     private AtomicInteger batchCounter = new AtomicInteger(0);
     private Map<Comparable, SenderWorker> waitingSenders = new ConcurrentHashMap<>(); // ackId -> SenderWorker
     private ByteBuffer buffer;
-    private final String eventsFilename = "./many_text_events_no_timestamp.sample";
+    private final String eventsFilename = "./1KB_event.sample";
     private long start = 0;
     private long testStartTimeMillis = System.currentTimeMillis();
     private long warmUpTimeMillis = 2*60*1000; // 2 mins
