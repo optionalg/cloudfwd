fcaa900387ffbda40495a237cbf5ff4f66c5cf5c
Geoffrey Hendrey <ghendrey@splunk.com>
2017-12-30 14:51:53 -0800

    Increase max connections per host

--- src/main/java/com/splunk/cloudfwd/impl/http/HttpClientWrapper.java
+++ src/main/java/com/splunk/cloudfwd/impl/http/HttpClientWrapper.java
@@ -75,9 +75,9 @@ public class HttpClientWrapper {
     }    
     
     private void adjustConnPoolSize(){        
-        httpClientAndConnPoolControl.getConPoolControl().setDefaultMaxPerRoute(Math.max(requestors.size(),HttpClientFactory.INITIAL_MAX_CONN_PER_ROUTE));
+        httpClientAndConnPoolControl.getConPoolControl().setDefaultMaxPerRoute(Math.max(requestors.size()*4,HttpClientFactory.INITIAL_MAX_CONN_PER_ROUTE));
         //We expect only one Route per HttpSender, but nevertheless, for safety we double the number of requestors in computing the max total connections. This is
         //a decent idea becuase for each HttpSender there will be multiple pollers (health, acks) in addition to event posting
-        httpClientAndConnPoolControl.getConPoolControl().setMaxTotal(Math.max(requestors.size()*2,HttpClientFactory.INITIAL_MAX_CONN_TOTAL));
+        httpClientAndConnPoolControl.getConPoolControl().setMaxTotal(Math.max(requestors.size()*8,HttpClientFactory.INITIAL_MAX_CONN_TOTAL));
     }
 }
