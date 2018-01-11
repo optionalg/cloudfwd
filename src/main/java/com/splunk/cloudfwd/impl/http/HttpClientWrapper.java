37d5581aa7a1a878bc4938d915d56be0f1084f4a
Eric Prokop <ecprokop@gmail.com>
2018-01-11 13:45:11 -0800

    whitespace

--- src/main/java/com/splunk/cloudfwd/impl/http/HttpClientWrapper.java
+++ src/main/java/com/splunk/cloudfwd/impl/http/HttpClientWrapper.java
@@ -74,7 +74,7 @@ public class HttpClientWrapper {
         return httpClientAndConnPoolControl.getClient();
     }    
     
-    private void adjustConnPoolSize(){
+    private void adjustConnPoolSize(){                
         httpClientAndConnPoolControl.getConPoolControl().setDefaultMaxPerRoute(Math.max(requestors.size()*10,HttpClientFactory.INITIAL_MAX_CONN_PER_ROUTE));
         //We expect only one Route per HttpSender, but nevertheless, for safety we double the number of requestors in computing the max total connections. This is
         //a decent idea becuase for each HttpSender there will be multiple pollers (health, acks) in addition to event posting
